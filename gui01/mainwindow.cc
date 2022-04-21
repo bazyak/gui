a70eaa88f113032170387d1a5031271d75bc4f71
Ruslan Bazyak <ruslanbazyak@gmail.com>
2022-04-21 20:01:57 +0300

    input check

--- gui01/mainwindow.cc
+++ gui01/mainwindow.cc
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -8,18 +10,20 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 }
 
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
 // Exercise 1
 void MainWindow::on_ex1_calc_button_clicked()
 {
-    auto a = ui->ex1_edit_a->text().toDouble();
-    auto b = ui->ex1_edit_b->text().toDouble();
-    auto d = ui->ex1_edit_d->text().toDouble();
-    auto c = ui->ex1_edit_c->text().toDouble() - d;
+    auto const a = ui->ex1_edit_a->text().toDouble();
+    auto const b = ui->ex1_edit_b->text().toDouble();
+    auto const d = ui->ex1_edit_d->text().toDouble();
+    auto const c = ui->ex1_edit_c->text().toDouble() - d;
 
     QString res = "";
     if (!a)
@@ -29,12 +33,12 @@ void MainWindow::on_ex1_calc_button_clicked()
     }
     else
     {
-        auto dis = b*b - 4*a*c;
+        auto const dis = b*b - 4*a*c;
 
         if (dis < 0) res = "корней нет";
         else
         {
-            auto den = 2*a;
+            auto const den = 2*a;
 
             res = !dis ? "x = "+ QString::number(-b / den)
                        : "x1 = " + QString::number((-b - qSqrt(dis)) / den) + ",  " +
@@ -44,40 +48,56 @@ void MainWindow::on_ex1_calc_button_clicked()
     ui->ex1_label_result->setText(res);
 }
 
+
+// Exercise 2
 void MainWindow::on_ex2_calc_button_clicked()
 {
     auto a = ui->ex2_edit_a->text().toDouble();
     auto b = ui->ex2_edit_b->text().toDouble();
     auto angle = ui->ex2_edit_angle->text().toDouble();
 
+    if (a <= 0 || b <= 0)
+    {
+        QMessageBox::critical(this, "Ошибка", "Сторона треугольника должна быть больше 0");
+        return;
+    }
+
+    if (!(angle > 0 && angle < 180))
+    {
+        QMessageBox::critical(this, "Ошибка", "Угол должен быть в диапазоне (0; 180)");
+        return;
+    }
+
     if (ui->ex2_select_deg->isChecked())
     {
         angle = qDegreesToRadians(angle);
     }
-    auto c = qSqrt(a*a + b*b - 2*a*b*qCos(angle));
+
+    double const _cos = angle > 90 ? -qCos(angle) : qCos(angle);
+
+    auto const c = qSqrt(a*a + b*b - 2*a*b*_cos);
 
     ui->ex2_label_result->setText(QString::number(c));
 }
 
 
+// Exercise 3
 void MainWindow::on_ex3_add_button_clicked()
 {
-    auto text = ui->ex3_plain_text_1->toPlainText();
+    auto const text = ui->ex3_plain_text_1->toPlainText();
     ui->ex3_plain_text_2->appendPlainText(text);
 }
 
 
 void MainWindow::on_ex3_replace_button_clicked()
 {
-    auto text = ui->ex3_plain_text_2->toPlainText();
+    auto const text = ui->ex3_plain_text_2->toPlainText();
     ui->ex3_plain_text_1->setPlainText(text);
 }
 
 
-
 void MainWindow::on_ex3_html_button_clicked()
 {
-    static QString const text = "<font color='red'><b>This is only a test</b></font>";
+    static const QString text = "<font color='red'><b>This is only a test</b></font>";
     ui->ex3_plain_text_2->appendHtml(text);
 }
-
