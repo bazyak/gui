da87d73762ac6686d6edac487704b2111cd0d186
Ruslan Bazyak <ruslanbazyak@gmail.com>
2022-05-05 19:42:01 +0300

    looks like done

---
+++ gui04/gui04_en.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_window.ui" line="14"/>
+        <location filename="main_window.cc" line="126"/>
+        <source>Урок 04</source>
+        <translation>Lesson 04</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="43"/>
+        <location filename="main_window.cc" line="138"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="59"/>
+        <location filename="main_window.cc" line="140"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="75"/>
+        <location filename="main_window.cc" line="139"/>
+        <source>Открыть на чтение</source>
+        <translation>Open for reading</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="91"/>
+        <location filename="main_window.cc" line="141"/>
+        <source>Справка</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="104"/>
+        <source>Русский</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="123"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="59"/>
+        <source>Выберите файл для сохранения</source>
+        <translation>Select file for saving</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="61"/>
+        <source>Текстовый файл (*.txt);;Любой файл (*.*)</source>
+        <translation>Text file (*.txt);;Any file (*.*)</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="65"/>
+        <source>Ошибка</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="65"/>
+        <source>Этот файл открыт только для чтения</source>
+        <translation>This is read only file</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="91"/>
+        <source>Помощь</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="103"/>
+        <source>Выберите файл для открытия</source>
+        <translation>Select file for opening</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="105"/>
+        <source>Текстовые файлы (*.txt);;Все файлы (*.*)</source>
+        <translation>Text files (*.txt);;All files (*.*)</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="121"/>
+        <source>[Только для чтения]</source>
+        <translation>[Read only]</translation>
+    </message>
+</context>
+</TS>
